30b6b4a295706aa757eb84893f221ec284ef213a
Chris Norman <chris.norman2@googlemail.com>
2020-04-15 19:09:34 +0100

    Added getText function.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,106 @@
-/* globals book, ConfirmPage, Game, gameJson, keyboardArea, Level, LevelObject, Line, mainDiv, message, ObjectTypes, Page, startAudio, startButton, startDiv, TtsPage */
+/* globals Book, ConfirmPage, Game, Level, LevelObject, Line, ObjectTypes, Page, startAudio, TtsPage */
+
+const startDiv = document.querySelector("#startDiv")
+const mainDiv = document.querySelector("#main")
+const keyboardArea = document.querySelector("#keyboardArea")
+const gameJson = document.querySelector("#gameJson")
+const startButton = document.querySelector("#startButton")
+const message = document.querySelector("#message")
+const book = new Book()
+
+const stringForm = document.querySelector("#stringForm")
+const stringPrompt = document.querySelector("#stringPrompt")
+const stringPromptDefaultValue = stringPrompt.innerText
+const stringInput = document.querySelector("#stringInput")
+const stringCancel = document.querySelector("#stringCancel")
+
+const textForm = document.querySelector("#textForm")
+const textPrompt = document.querySelector("#textPrompt")
+const textPromptDefaultValue = textPrompt.innerText
+const textInput = document.querySelector("#textInput")
+const textCancel = document.querySelector("#textCancel")
+
+function getText(obj) {
+  // Pass a dictionary with the following keys:
+  //
+  // book: The book that called this function. This value must be provided.
+  // multiline: Sets whether or not to show a single line, or multiline text field. Defaults to false.
+  // prompt: The prompt text to show. Defaults to textPromptDefaultValue if multiline is true, or stringPromptDefaultValue otherwise.
+  // value: The default value. Defaults to an empty string.
+  // onok: The callback to be called as onok(value, book), where value is the value that was entered. Defaults to book.message.
+  // oncancel: A callback to be called as oncancel(form, book), where form is the form that was chosen based on the value of multiline. Defaults to a callback that hides the form.
+  obj.multiline = obj.multiline || false
+  if (!obj.prompt) {
+    if (obj.multiline) {
+      obj.prompt = textPromptDefaultValue
+    } else {
+      obj.prompt = stringPromptDefaultValue
+    }
+  }
+  obj.value = obj.value || ""
+  if (!obj.onok) {
+    obj.onok = (text) => {
+      obj.book.message(text)
+    }
+  }
+  if (!obj.oncancel) {
+    obj.oncancel = (f, b) => {
+      inputElement.value = ""
+      f.hidden = true
+      keyboardArea.focus()
+      b.showFocus()
+    }
+  }
+  let form, promptElement, inputElement, cancelElement = null
+  if (obj.multiline) {
+    form = textForm
+    promptElement = textPrompt
+    inputElement = textInput
+    cancelElement = textCancel
+  } else {
+    form = stringForm
+    promptElement = stringPrompt
+    inputElement = stringInput
+    cancelElement = stringCancel
+  }
+  cancelElement.onclick = () => obj.oncancel(form, obj.book)
+  form.onkeydown = (e) => {
+    if (e.key == "Escape") {
+      e.preventDefault()
+      cancelElement.click()
+    }
+  }
+  inputElement.value = obj.value
+  inputElement.setSelectionRange(0, -1)
+  promptElement.innerText = obj.prompt
+  form.hidden = false
+  form.onsubmit = (e) => {
+    e.preventDefault()
+    form.hidden = true
+    obj.book.showFocus()
+    obj.onok(inputElement.value, obj.book)
+    keyboardArea.focus()
+  }
+  inputElement.focus()
+}
 
 function EditLevelMenu(b, level) {
   const lines = [
     new Line(
       "Rename", (b) => {
-        const title = prompt("Enter new level name", level.title)
-        if (title && title != level.title) {
-          level.title = title
-          b.message("Level renamed.")
-        }
+        getText(
+          {
+            book: b,
+            prompt: "Enter new level name",
+            value: level.title,
+            onok: (title, bk) => {
+              if (title && title != level.title) {
+                level.title = title
+                bk.message("Level renamed.")
+              }
+            }
+          }
+        )
       }
     ),
     new Line(
@@ -21,13 +113,23 @@ function EditLevelMenu(b, level) {
     const description = level.numericProperties[name]
     lines.push(
       new Line(
-        () => `${description} (${level[name]})`, (b) => {
-          let value = Number(prompt("Enter new value", level[name])) || level[name]
-          if (isNaN(value)) {
-            b.message("Invalid number.")
-          } else {
-            level[name] = value
-          }
+        () => `${description} (${level[name]})`, () => {
+          getText(
+            {
+              book: b,
+              prompt: "Enter new value",
+              value: level[name],
+              onok: (value, bk) => {
+                value = Number(value) || level[name]
+                if (isNaN(value)) {
+                  bk.message("Invalid number.")
+                } else {
+                  level[name] = value
+                }
+                bk.showFocus()
+              }
+            }
+          )
         }
       )
     )
@@ -37,12 +139,21 @@ function EditLevelMenu(b, level) {
     lines.push(
       new Line(
         () => `${description} (${level[name] === null ? "not set" : level[name]})`, () => {
-          const url = prompt("Enter a URL", level[name] || "")
-          if (url) {
-            level[name] = url
-          } else {
-            level[name] = null
-          }
+          getText(
+            {
+              book: b,
+              prompt: "Enter a URL",
+              value: level[name] || "",
+              onok: (url, bk) => {
+                if (url) {
+                  level[name] = url
+                } else {
+                  level[name] = null
+                }
+                bk.showFocus()
+              }
+            }
+          )
         }
       )
     )
@@ -108,12 +219,32 @@ function EditObjectMenu(b, obj) {
   const lines = [
     new Line(
       "Rename", () => {
-        obj.title = prompt("New title", obj.title) || obj.title
+        getText(
+          {
+            book: b,
+            prompt: "New title",
+            value: obj.title,
+            onok: (title, bk) => {
+              obj.title = title || obj.title
+              bk.showFocus()
+            }
+          }
+        )
       }
     ),
     new Line(
       () => `Sound URL (${obj.soundUrl})`, () => {
-        obj.soundUrl = prompt("New URL", obj.soundUrl) || null
+        getText(
+          {
+            book: b,
+            prompt: "New URL",
+            value: obj.soundUrl,
+            onok: (url, bk) => {
+              obj.soundUrl = url || null
+              bk.showFocus()
+            }
+          }
+        )
       }
     ),
     new Line(
@@ -153,14 +284,21 @@ function ObjectsMenu(b) {
   const lines = [
     new Line(
       "Add Object", (b) => {
-        const title = prompt("Enter the name for the new object") || ""
-        if (title) {
-          const obj = new Object()
-          obj.title = title
-          b.game.objects.push(obj)
-          b.pop()
-          b.push(ObjectsMenu(b))
-        }
+        getText(
+          {
+            book: b,
+            prompt: "Enter the name for the new object",
+            onok: (title, bk) => {
+              if (title) {
+                const obj = new Object()
+                obj.title = title
+                bk.game.objects.push(obj)
+                bk.pop()
+                bk.push(ObjectsMenu(bk))
+              }
+            }
+          }
+        )
       }
     ),
   ]
@@ -192,7 +330,16 @@ startButton.onclick = () => {
         lines: [
           new Line(
             "Set Game Name", (b) => {
-              b.game.title = prompt("Enter a new name", b.game.title || "Untitled Game")
+              getText(
+                {
+                  book: b,
+                  prompt: "Enter a new name",
+                  value: b.game.title,
+                  onok: (text, bk) => {
+                    bk.game.title = text || bk.game.title
+                  }
+                }
+              )
             }
           ),
           new Line(
@@ -207,12 +354,22 @@ startButton.onclick = () => {
           ),
           new Line(
             "Set Volume Change Amount", (b) => {
-              const value = Number(prompt("Enter new value", b.game.volumeChangeAmount), 2)
-              if (isNaN(value)) {
-                b.message("Invalid value.")
-              } else {
-                b.game.volumeChangeAmount = Number(value, 2) || b.game.volumeChangeAmount
-              }
+              getText(
+                {
+                  book: b,
+                  prompt: "Enter new value",
+                  value: b.game.volumeChangeAmount,
+                  onok: (value, bk) => {
+                    value = Number(value, 2)
+                    bk.showFocus()
+                    if (isNaN(value)) {
+                      bk.message("Invalid value.")
+                    } else {
+                      bk.game.volumeChangeAmount = Number(value, 2) || b.game.volumeChangeAmount
+                    }
+                  }
+                }
+              )
             }
           ),
           new Line(
@@ -268,7 +425,9 @@ startButton.onclick = () => {
 }
 
 window.onload = () => {
-  mainDiv.hidden = true
+  for (let e of [mainDiv, stringForm, textForm]) {
+    e.hidden = true
+  }
 }
 
 keyboardArea.onkeydown = (e) => {
@@ -306,9 +465,8 @@ keyboardArea.onkeydown = (e) => {
         )
       }
     } else {
-      return book.onkeydown(e)
+      book.onkeydown(e)
     }
-    e.stopPropagation()
   } catch(e) {
     book.message(e)
     throw(e)
